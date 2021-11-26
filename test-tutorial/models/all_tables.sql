cd0d2f2c3d353e93b6f7af236c1360562d732ddb
Christophe Kevers <christophe.kevers@luminus.be>
2021-11-26 09:28:34 +0100

    test for stable start

--- test-tutorial/models/all_tables.sql
+++ test-tutorial/models/all_tables.sql
@@ -2,10 +2,14 @@ with winners as (
 
     select * {{ ref('stg_winners') }}
 
-), 
-
-losers as (
-
-    select * from {{ ref('stg_losers')}}
-
 )
+
+-- losers as (
+
+--     select * from {{ ref('stg_losers') }}
+
+-- )
+
+select * 
+from stg_winners
+-- inner join stg_losers
---
+++ test-tutorial/models/stg_losers.sql
@@ -0,0 +1,1 @@
+select C19 from "PC_DBT_DB"."DBT_CKEVERS".app_data
